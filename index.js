56b25bf4c28058828e3384586f40abbcbbade342
javier920323 <javiers920323@gmail.com>
2022-09-26 13:49:17 -0500

    vamos2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require('cors');
 require('dotenv').config()
 const stripe = require('stripe')(process.env.STRIPE_CLIENT_SECRET)
-
+console.log(process.env.STRIPE_CLIENT_SECRET)
 const port = process.env.PORT || 4242;
 
 const app = express();
