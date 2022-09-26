93556b00de8900160668a4bfea409d1ab3eb5dd9
javier920323 <javiers920323@gmail.com>
2022-09-26 15:22:35 -0500

    ya

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ const app = express();
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://amazon-clone-javier.vercel.app"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.listen(port)
 
@@ -23,7 +18,8 @@ app.post('/payment/create', async (req, res) => {
   // se guarda en 'total' lo que se solicitud('req') mediante a la url con el query en este caso el total a pagar
   const total = req.query.total;
   console.log('BOOM', total)
-
+  res.header("Access-Control-Allow-Origin", "https://amazon-clone-javier.vercel.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   //funcion 'paymentIntent' que contine todos los objetos de la funcion 'stripe.paymentIntents.create' en este caso se asgina el amount y la currency 
   const paymentIntent = await stripe.paymentIntents.create({
     amount: total,
